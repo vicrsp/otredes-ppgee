cc3224f09a95ccbd10faa997c421837b0c53c497
Victor Ruela <victorspruela@gmail.com>
2020-08-21 20:55:35 -0300

    - makespan modelado com cplex

--- aula-computacional/aula_p2_makespan.py
+++ aula-computacional/aula_p2_makespan.py
@@ -4,8 +4,7 @@ import gurobipy as gp
 from gurobipy import GRB, quicksum
 import matplotlib.pyplot as plt
 
-
-#%%  DESIGNAÇÂO GENERALIZADA
+#%%  MAKESPAN - GUROBI
 try:
 
     # Create a new model
@@ -60,7 +59,42 @@ except AttributeError:
     print('Encountered an attribute error')
 
 
+#%%  MAKESPAN - CPLEX
+import cplex
+from cplex.exceptions import CplexError
+from docplex.mp.model import Model
+
+makespan_prob = Model(name='makespan')
+
+x = makespan_prob.binary_var_matrix(range(m), range(n), name='x')
+y = makespan_prob.integer_var(lb=0, name='y')
+
+# Constraint #1: machine allocation should be below makespan
+for i in range(m):
+    makespan_prob.add_constraint(makespan_prob.sum(x[i,j] * p[i,j] for j in range(n)) <= y)
+
+# Constrain #2: each job should be scheduled once
+for j in range(n):
+    makespan_prob.add_constraint(makespan_prob.sum(x[i,j] for i in range(m)) == 1)
+
+makespan_prob.minimize(y)
+
+makespan_prob.solve()
+makespan_prob.print_information()
+makespan_prob.print_solution()
+
+makespan_prob.export_as_lp('D:\\otredes-ppgee\\aula-computacional\\makespan_cplex.lp')
+image = np.zeros((m, n))
+for i in range(m):
+    for j in range(n):
+        image[i,j] = makespan_prob.get_var_by_name('x_{}_{}'.format(i,j))
+    
+plt.matshow(image)
+plt.yticks(range(m), range(m))
+plt.xticks(range(n), range(n))
+
+plt.show()
 
 
 
-
+# %%
