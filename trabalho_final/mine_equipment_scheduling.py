eae3ef7c08d473c0bf14345dffe21ff91c9efef7
Victor Ruela <victorspruela@gmail.com>
2020-09-02 21:41:56 -0300

    - corrigindo problema com alocação das bins e custo repetido

--- trabalho_final/mine_equipment_scheduling.py
+++ trabalho_final/mine_equipment_scheduling.py
@@ -2,15 +2,17 @@
 from docplex.mp.model import Model
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 model = Model(name='mine_schedule')
-model.parameters.mip.tolerances.mipgap = 0.01
+model.parameters.mip.tolerances.mipgap = 0.1
 #%% ARTIFICIAL INSTANCE DATA
 n_trucks = 34 # The number of trucks
 n_years = 10 # The time period
 n_bins = 20 # The number of age bins
 planned_production = 365 * 24 * n_trucks # the entire year non stop
-age_bin_size = 5000
+#age_bin_size = 5000
+M = 5000
 min_truck_availability = planned_production / n_trucks
 
 def get_cost_matrix(n_trucks, n_bins, n_years, cost_type='random', critical_bin = 15):
@@ -39,6 +41,8 @@ def get_production_targets(n_years, target_type='random'):
 def get_initial_ages(n_trucks, ages_type='random'):
     if(ages_type == 'random'):
         return np.random.randint(low=0, high=20000, size = n_trucks)
+    if(ages_type == 'zero'):
+        return np.zeros(n_trucks)
     # if(ages_type == 'paper'): TODO: copy from paper
 
 # Discounted cost value for truck T at age bin B and period T
@@ -49,13 +53,13 @@ FE = [750000] * n_trucks
 A = np.random.randint(low=min_truck_availability*0.9, high=min_truck_availability, size = (n_trucks, n_years))
 #np.ones((n_trucks, n_years)) * min_truck_availability
 # Maximum available truck hours at age bin V
-M = np.arange(start=1, stop=n_bins+1) * age_bin_size
+#M = np.arange(start=1, stop=n_bins+1) * age_bin_size
 # The cumulative used hours for truck t at time period t
 # H = np.random.randint(low=0, high=1000, size = (n_trucks, n_years))
 # The required truck hours for a given time period y
 R = get_production_targets(n_years)
 # The initial truck ages
-InitialAge = get_initial_ages(n_trucks)
+InitialAge = get_initial_ages(n_trucks, 'zero')
 # The critical age bin
 c_critical = 15
 
@@ -66,7 +70,9 @@ h = model.integer_var_matrix(range(n_trucks), range(n_years), name='h')
 
 #%% CPLEX MODEL
 # Objective: TODO: revisar se não está adicionando FE a mais
-model.minimize(model.sum(x[t,b,y] * C[t,b,y] + y_bin[t,c_critical,y]*(FE[t]/(n_trucks*n_bins*n_years)) for t in range(n_trucks) for b in range(n_bins) for y in range(n_years)))
+hour_costs = model.sum(x[t,b,y] * C[t,b,y] for t in range(n_trucks) for b in range(n_bins) for y in range(n_years))
+repair_costs = model.sum(y_bin[t,c_critical,y]*FE[t] for t in range(n_trucks) for y in range(n_years))
+model.minimize(model.sum(hour_costs + repair_costs))
 
 # Constraints
 # (1) - Trucks maximum availability
@@ -76,20 +82,20 @@ for y in range(n_years):
 # (2) - Ensure move to upper bin 
 for t in range(n_trucks):
     for b in range(n_bins): 
-        model.add_constraint(model.sum(x[t,b,y] for y in range(n_years)) <= M[b])
+        model.add_constraint(model.sum(x[t,b,y] for y in range(n_years)) <= M)
 # (3) - Accumulated truck age
 
 # (4) - Correct bin order (lower bound)
 for t in range(n_trucks):
     for b in range(n_bins): 
         for y in range(n_years): 
-            model.add_constraint(model.sum(x[t,b,k] for k in range(y+1)) - M[b] * y_bin[t,b,y] >= 0)
+            model.add_constraint(model.sum(x[t,b,k] for k in range(y+1)) - M * y_bin[t,b,y] >= 0)
 
 # (5) - Correct bin order (upper bound)
 for t in range(n_trucks):
     for b in range((n_bins - 1)): # we can skip the last bin
         for y in range(n_years): 
-            model.add_constraint(x[t,(b+1),y] - M[(b+1)]* model.sum(y_bin[t,b,k] for k in range(y+1)) <= 0)
+            model.add_constraint(x[t,(b+1),y] - M * model.sum(y_bin[t,b,k] for k in range(y+1)) <= 0)
 
 # (6) - Required truck yours per year
 for y in range(n_years):
@@ -103,8 +109,8 @@ for y in range(n_years):
         else:
             model.add_constraint(model.sum(x[t,b,y] for b in range(n_bins)) + h[t,y-1] == h[t,y])
 
-model.export_as_lp('D:\\otredes-ppgee\\trabalho_final\\test.lp')
-model.export_as_mps('D:\\otredes-ppgee\\trabalho_final\\test_mps.mps')
+#model.export_as_lp('D:\\otredes-ppgee\\trabalho_final\\test.lp')
+#model.export_as_mps('D:\\otredes-ppgee\\trabalho_final\\test_mps.mps')
 
 #%% SOLVE
 solution = model.solve(log_output=True)
@@ -115,10 +121,12 @@ if(solution):
     #model.print_solution()
 
     # Plot the accumulated hours
-    fig, ax = plt.subplots(2,1, figsize=(16,10))
+    fig, ax = plt.subplots(3,1, figsize=(16,10))
 
     image_h = np.zeros((n_years, n_trucks))
     image_hours = np.zeros((n_years, n_trucks))
+    image_bins = np.zeros((n_bins, n_trucks))
+    image_y_critical = np.zeros((n_years, n_trucks))
     for i in range(n_years):
         for j in range(n_trucks):
             image_h[i,j] = model.get_var_by_name('h_{}_{}'.format(j,i))
@@ -130,11 +138,27 @@ if(solution):
                 hours = hours + int(model.get_var_by_name('x_{}_{}_{}'.format(j,b,i)))
             image_hours[i,j] = hours
 
-    ch = ax[0].matshow(image_h, cmap='Greys')
-    cho = ax[1].matshow(image_hours,cmap='Reds')
+    for i in range(n_bins):
+        for j in range(n_trucks):
+            hours = 0
+            for y in range(n_years):
+                hours = hours + int(model.get_var_by_name('x_{}_{}_{}'.format(j,i,y)))
+            image_bins[i,j] = hours
 
-    fig.colorbar(ch, ax=ax[0])
-    fig.colorbar(cho, ax=ax[1])
+    for i in range(n_years):
+        for j in range(n_trucks):
+            image_y_critical[i,j] = int(model.get_var_by_name('y_bin_{}_{}_{}'.format(j,c_critical,i)))
+
+    #ch = ax[0].matshow(image_h, cmap='Greys')
+    #cho = ax[1].matshow(image_hours,cmap='Reds')
+    sns.heatmap(image_h / 1000, annot=True, ax=ax[0])
+    sns.heatmap(image_hours / 1000, annot=True,ax=ax[1])
+    sns.heatmap(image_bins / 1000, annot=True, ax=ax[2])
+
+    #fig.colorbar(ch, ax=ax[0])
+    #fig.colorbar(cho, ax=ax[1])
+    #fig.colorbar(chy, ax=ax[2])
+
 
     plt.tight_layout()
     plt.show()
